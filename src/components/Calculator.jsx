478dd4cdf1f69ec511f762f25ca3d5755db2f867
Jesus Cervantes <jesusuper9@gmail.com>
2023-01-23 20:54:09 -0600

    fix: update card design

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,21 +8,23 @@ export const Calculator = () => {
 
   return (
     <div className="p-20">
-      <h1 className="text-center text-4xl">What items to store?</h1>
-      <p className="m-auto text-center w-[453px] text-sm">
-        Select which items you wish to store before moving to your new home.
-        Well keep em safe!
-      </p>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-10">
+        <h1 className="text-center text-4xl">What items to store?</h1>
+        <p className="m-auto text-center w-[453px] text-sm">
+          Select which items you wish to store before moving to your new home.
+          Well keep em safe!
+        </p>
+      </div>
+      <div className="flex flex-col gap-14 mt-10">
         <div className="flex gap-20">
           {items.map((item) => {
             return (
-              <div key={item.id} className="w-[220px] text-sm shadow-lg">
-                <div className="flex flex-col items-center justify-center gap-3">
+              <div key={item.id} className="text-sm">
+                <div className="shadow-lg flex flex-col items-center justify-center gap-3 w-[220px] h-[220px]">
                   <img src={item.image} alt="" />
                   <p>{item.label}</p>
                 </div>
-                <div className="flex h-[50px]">
+                <div className="flex h-[50px] mt-6">
                   <button
                     className="min-w-[41px] bg-gray-200"
                     onClick={() => handleDecrease(item)}
@@ -44,7 +46,7 @@ export const Calculator = () => {
           })}
         </div>
         <button
-          className="border border-orange-500 px-10 py-3 w-min"
+          className="border border-orange-500 px-10 py-3 w-min rounded-sm text-sm"
           onClick={() => {
             handleClear()
           }}
