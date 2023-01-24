562f851342984a15a1e23e2ee8cce1bad016b04c
Jesus Cervantes <jesusuper9@gmail.com>
2023-01-23 22:10:51 -0600

    fix: remove magic numbers from item list and add random id

--- src/context/items.js
+++ src/context/items.js
@@ -1,21 +1,34 @@
 import BedIcon from "../assets/beds.png"
+import FurnitureIcon from "../assets/furniture.png"
 import RefrigeratorIcon from "../assets/refrigerator.png"
+
+const INITIAL_AMOUNT = 0
+
+const randomId = () => window.crypto.randomUUID()
 
 export const initialItems = [
   {
+    id: randomId(),
     slug: "beds",
     label: "Beds",
     image: BedIcon,
     meters: 1.2,
-    id: 1,
-    amount: 0
+    amount: INITIAL_AMOUNT
   },
   {
-    id: 2,
+    id: randomId(),
     slug: "refri",
     label: "Refrigerator",
     image: RefrigeratorIcon,
     meters: 1,
-    amount: 0
+    amount: INITIAL_AMOUNT
+  },
+  {
+    id: randomId(),
+    slug: "furniture",
+    label: "Furniture",
+    image: FurnitureIcon,
+    meters: 0.5,
+    amount: INITIAL_AMOUNT
   }
 ]
